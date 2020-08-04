aefbe99f0bc08db011cec144034c65b4ec82bfdf
Ryan Delaney <vitineth@gmail.com>
2020-08-04 23:35:13 +0100

    setting up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    indent: ['error', 4, {
+      'SwitchCase': 1,
+    }],
+    'max-len': ['error', 120],
+    'padded-blocks': 'off',
+    'object-curly-newline': ['error', {
+      'ImportDeclaration': 'never',
+    }],
+    'linebreak-style': 'off',
+
+    'no-unused-vars': 'off',
+    'no-restricted-syntax': 'off',
+    'no-use-before-define': 'off',
+    'no-plusplus': ['error', {
+        allowForLoopAfterthoughts: true,
+    }],
+    'no-underscore-dangle': 'off',
+    'no-continue': 'off',
+  }
+};
