105f470a27f829ca182f68f55b1df7e12d89681d
Ryan Delaney <vitineth@gmail.com>
2020-08-05 22:22:19 +0100

    Updating new log format and exports
    It now handles additional metadata and messages with labels, a utility function for producing labelled loggers etc, this now produces nice coloured outputs on the console. I've also added a UEMS environment variable which controls whether we write to a file to cut down on log file bloat during development

--- src/logging/Log.ts
+++ src/logging/Log.ts
@@ -1,10 +1,16 @@
 import winston from "winston";
+import { has } from "../utilities/ObjectUtilities";
+import util from 'util';
 
 /**
  * The current environment simplified to either dev or prod only. If NODE_ENV is not set to dev we are assumed to be in
  * production
  */
 const environment = process.env.NODE_ENV === 'dev' ? 'dev' : 'prod';
+/**
+ * Determines if the file output should be used, if UEMS contains a NO_FILE property, this will be false
+ */
+const useFile = !(process.env.UEMS && (/^NO_FILE$|^NO_FILE;|;NO_FILE$|;NO_FILE;/i).test(process.env.UEMS));
 /**
  * The current date at the time this file is loaded
  */
@@ -15,30 +21,85 @@ const now = new Date();
 const nowString = `${now.getUTCFullYear()}-${now.getUTCMonth()}-${now.getUTCDay()}`;
 
 /**
+ * The file transport for the default log if enabled
+ */
+const fileTransport = [new winston.transports.File({
+    dirname: 'logs',
+    filename: `summary-${environment}.${nowString}.log`,
+    format: winston.format.json(),
+})];
+
+const formatter = winston.format.printf(({ level, message, ...meta }) => {
+    const timestamp = has(meta, 'timestamp') ? meta['timestamp'] as string : new Date().toISOString();
+    const label = has(meta, 'metadata') && has(meta.metadata, 'label')
+        ? (meta.metadata as Record<string, string>).label
+        : undefined;
+
+    let result = `${timestamp}`;
+    if (label) {
+        result = `${result} [${label}] ${level}: ${message}`;
+    } else {
+        result = `${result} ${level}: ${message}`;
+    }
+
+    let attachment = '';
+    if (has(meta, 'metadata')) {
+        for (const [key, value] of Object.entries(meta.metadata)) {
+            if (key === 'label') continue;
+
+            attachment += `\n[${key}]: ${util.inspect(value, false, null)}`;
+        }
+    }
+
+    attachment = attachment.split('\n').map((line) => `\t${line}`).join('\n');
+    if (attachment.trim().length !== 0) {
+        result += `${attachment}`;
+    }
+
+    return result;
+});
+/**
  * Constructs a logger either at the info or debug level in the logs folder. If in a development environment, it will
  * add a transport to log to the console.
  */
 const logger = winston.createLogger({
-    level: environment === 'dev' ? 'debug' : 'info',
+    level: environment === 'dev' ? 'silly' : 'info',
     transports: [
-        new winston.transports.File({
-            dirname: 'logs',
-            filename: `summary-${environment}.${nowString}.log`,
-            format: winston.format.json(),
-        }),
+        ...(useFile ? fileTransport : []),
         ...(environment === 'prod' ? [] : [
             new winston.transports.Console({
-                format: winston.format.simple(),
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.metadata(),
+                    winston.format.timestamp(),
+                    winston.format.label(),
+                    formatter,
+                ),
+                level: 'silly',
             }),
         ]),
     ],
+    handleExceptions: false,
 });
+
+/**
+ * Constructs a child logger of {@link logger} which attaches the given label
+ * @param label
+ */
+const makeLogger = (label: string): winston.Logger => (logger.child({
+    label,
+}));
 
 /**
  * Default export is logger described by {@link logger}. This will log to console or file depending on environment with
  * either debug or info level.
  */
-export default logger;
+export default makeLogger;
+
+/**
+ * Wrapper export around {@link makeLogger}
+ */
+export const _ml = makeLogger;
 
 /**
  * Utility named export, see {@link logger}.
