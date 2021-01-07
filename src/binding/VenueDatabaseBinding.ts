c814ba04be2aaba5d882e8367aefff0967e2fe90
Ryan Delaney <vitineth@gmail.com>
2021-01-07 23:04:18 +0000

    Bumping uems and fixing bugs

--- src/binding/VenueDatabaseBinding.ts
+++ src/binding/VenueDatabaseBinding.ts
@@ -52,6 +52,7 @@ async function execute(
             msg_id: message.msg_id,
             status,
             result: result as VenueResponse.InternalVenue[],
+            userID: message.userID,
         });
     } else {
         send({
@@ -59,6 +60,7 @@ async function execute(
             msg_id: message.msg_id,
             status,
             result: result as string[],
+            userID: message.userID,
         });
     }
 }
