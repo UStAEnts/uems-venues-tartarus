361a25ba3a5750a1860cf2c083345741c0e24d2b
Ryan Delaney <vitineth@gmail.com>
2021-01-13 00:14:06 +0000

    Updating response codes and manually handling client facing errors

--- src/binding/VenueDatabaseBinding.ts
+++ src/binding/VenueDatabaseBinding.ts
@@ -1,8 +1,9 @@
 import { constants } from 'http2';
-import { VenueMessage, VenueResponse } from '@uems/uemscommlib';
+import { MsgStatus, VenueMessage, VenueResponse } from '@uems/uemscommlib';
 import { VenueDatabase } from '../database/Database';
 import { RabbitNetworkHandler } from '../networking/Messaging';
 import { _ml } from '../logging/Log';
+import { ClientFacingError } from "@uems/micro-builder/build/errors/ClientFacingError";
 
 const _b = _ml(__filename, 'binding');
 
@@ -23,27 +24,46 @@ async function execute(
         switch (message.msg_intention) {
             case 'CREATE':
                 result = await database.create(message);
-                status = constants.HTTP_STATUS_OK;
+                status = MsgStatus.SUCCESS;
                 break;
             case 'DELETE':
                 result = await database.delete(message);
-                status = constants.HTTP_STATUS_OK;
+                status = MsgStatus.SUCCESS;
                 break;
             case 'READ':
                 result = await database.query(message);
-                status = constants.HTTP_STATUS_OK;
+                status = MsgStatus.SUCCESS;
                 break;
             case 'UPDATE':
                 result = await database.update(message);
-                status = constants.HTTP_STATUS_OK;
+                status = MsgStatus.SUCCESS;
                 break;
             default:
-                status = constants.HTTP_STATUS_BAD_REQUEST;
+                status = constants.HTTP_STATUS_NOT_IMPLEMENTED;
         }
     } catch (e) {
         _b.error('failed to query database for events', {
             error: e as unknown,
         });
+
+        if (e instanceof ClientFacingError) {
+            send({
+                userID: message.userID,
+                status: MsgStatus.FAIL,
+                msg_intention: message.msg_intention,
+                msg_id: message.msg_id,
+                result: [e.message],
+            });
+        } else {
+            send({
+                userID: message.userID,
+                status: constants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
+                msg_intention: message.msg_intention,
+                msg_id: message.msg_id,
+                result: ['internal server error'],
+            });
+        }
+        return;
     }
 
     if (message.msg_intention === 'READ') {
